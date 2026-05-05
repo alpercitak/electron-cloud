2d2de6f68572f44299ee11352da0c0e2aadc4494
Alper Çıtak <citak.alper@gmail.com>
2026-05-05 14:05:42 +0200

    added description

--- src/components/hud/index.tsx
+++ src/components/hud/index.tsx
@@ -5,6 +5,7 @@ export default function Hud() {
     <div className={styles.hud}>
       <div className={styles.hud__panel}>
         <h1 className={styles.hud__title}>Electron Cloud</h1>
+        <p className={styles.hud__description}>Realtime orbital cloud simulation</p>
       </div>
     </div>
   );
